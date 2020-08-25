0a5e4e801a0d7397ea46868b5630d4e502bfe9b9
Hemanta Poudel <42676824+hemantapoudel@users.noreply.github.com>
2020-08-25 16:39:36 +0545

    Update settings.py

--- electornics/settings.py
+++ electornics/settings.py
@@ -130,6 +130,6 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_HOST_USER='feedmymind18@gmail.com'
-EMAIL_HOST_PASSWORD='Feed0041432N'
+EMAIL_HOST_PASSWORD='**********'
 EMAIL_USE_TLS=True
 EMAIL_USE_SSL=False
